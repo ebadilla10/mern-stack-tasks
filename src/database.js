95ab1488e8f0ca2fa4bba0eeedda23ac7f464a22
Esteban Badilla A <ebadilla10@gmail.com>
2021-07-11 21:52:48 -0600

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://localhost/mern-crud-test';
+const URI = 'mongodb+srv://badi:iq42v2-RfMrCupK@freedb-ebadilla10.ffucz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 mongoose.connect(URI)
   .then(db => console.log('Db is connected'))
